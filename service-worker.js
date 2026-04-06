c4fab7b86143ca1227e65557f2cf1b7ed17df766
Josué <josuevivanco@gmail.com>
2026-04-06 12:32:13 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = "trip-app-static-v18";
-const TICKETS_CACHE = "trip-app-tickets-v18";
+const STATIC_CACHE = "trip-app-static-v19";
+const TICKETS_CACHE = "trip-app-tickets-v19";
 
 const STATIC_ASSETS = [
   "index.html",
